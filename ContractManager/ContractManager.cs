96fda87cf61509a0af01e20c856b9fddc6243fc9
Floris Gaisser <f.gaisser@gmail.com>
2025-12-21 21:23:24 +0900

    Basic tutorial with opening a window.

---
+++ ContractManager/ContractManager.cs
@@ -0,0 +1,36 @@
+﻿using Brutal.ImGuiApi;
+using KSA;
+using StarMap.API;
+
+namespace ContractManager
+{
+[StarMapMod]
+public class ContractManager
+{
+    [StarMapImmediateLoad]
+    public void onImmediateLoad(Mod definingMod)
+    {
+        Console.WriteLine("[CM] 'onImmediateLoad'");
+    }
+
+    [StarMapAllModsLoaded]
+    public void OnAllModsLoaded()
+    {
+        Console.WriteLine("[CM] 'OnAllModsLoaded'");
+    }
+
+    [StarMapAfterGui]
+    public void AfterGui(double dt)
+    {
+        // Create the UI here
+
+        // Contract Manager Window
+        if (ImGui.Begin("Contract Manager", ImGuiWindowFlags.None))
+        {
+            ImGui.Text("This is the Contract Manager window.");
+        }
+        ImGui.End();
+    }
+}
+
+}  // End of ContractManager namespace
