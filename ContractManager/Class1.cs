961af8402d116af59fc25ee625859107bab7fa27
Floris Gaisser <f.gaisser@gmail.com>
2025-12-21 19:38:00 +0900

    Add project files.

---
+++ ContractManager/Class1.cs
@@ -0,0 +1,15 @@
+﻿using KSA;
+using StarMap.API;
+
+namespace ContractManager
+{
+    [StarMapMod]
+    public class ContractManager
+    {
+        [StarMapImmediateLoad]
+        public void Init(Mod definingMod)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
