a2fd39164e7d22126223d5ed44b6083785536c49
Floris Gaisser <f.gaisser@gmail.com>
2025-12-27 22:06:12 +0900

    chore: cleaned up code for contract management. feature: code to check prerequisites and determine if it is possible to offer a new contract.

--- ContractManager/ContractManager.cs
+++ ContractManager/ContractManager.cs
@@ -26,6 +26,14 @@ public class ContractManager
     [XmlElement("finishedContracts")]
     public List<Contract.Contract> finishedContracts {  get; set; } = new List<Contract.Contract>();
 
+    // Global ContractManager config of max number of contracts that can be offered simultaneously. Should be determined by the launch site management building.
+    [XmlElement("maxNumberOfOfferedContracts")]
+    public int maxNumberOfOfferedContracts { get; set; } = 2;
+    
+    // Global ContractManager config of max number of contracts that can be accepted simultaneously. Should be determined by the launch site management building.
+    [XmlElement("maxNumberOfAcceptedContracts")]
+    public int maxNumberOfAcceptedContracts { get; set; } = 1;
+
     // Internal fields
     private double _lastUpdateTime = 0.0d;
     private double _updateInterval = 5.0d;
@@ -50,13 +58,9 @@ public class ContractManager
         blueprintContract1.WriteToConsole();
         this._contractBlueprints.Add(blueprintContract1);
 
-        var dummyContract = new Contract.Contract(in blueprintContract1, 0.0d);
-        this.offeredContracts.Add(dummyContract);
-
         // For testing: create and write an example contract to disk
-        CreateExample001Contract();
+        //CreateExample001Contract();
     }
-
 
     [StarMapAfterGui]
     public void AfterGui(double dt)
@@ -65,55 +69,10 @@ public class ContractManager
         KSA.Vehicle currentVehicle = Program.ControlledVehicle;
         double playerTime = Program.GetPlayerTime();
 
-        if (playerTime - this._lastUpdateTime > this._updateInterval)
-        {
-            this._lastUpdateTime = playerTime;
-            Console.WriteLine($"[CM] Game time: {playerTime}s offered: {this.offeredContracts.Count} accepted: {this.acceptedContracts.Count} finished: {this.finishedContracts.Count}");
-            foreach (Contract.Contract offeredContract in this.offeredContracts)
-            {
-                // auto-accept for now
-                if (offeredContract.status == Contract.ContractStatus.Offered)
-                {
-                    offeredContract.AcceptOfferedContract(playerTime);
-                    acceptedContracts.Add(offeredContract);
-                }
-            }
-            // Cleanup offered contracts
-            for (int offeredContractIndex = 0; offeredContractIndex < offeredContracts.Count; offeredContractIndex++)
-            {
-                if (this.offeredContracts[offeredContractIndex].status != Contract.ContractStatus.Offered)
-                {
-                    this.offeredContracts.RemoveAt(offeredContractIndex);
-                    offeredContractIndex--;
-                }
-            }
-            foreach (Contract.Contract acceptedContract in this.acceptedContracts)
-            {
-                if (currentVehicle != null)
-                {
-                    acceptedContract.UpdateStateWithVehicle(currentVehicle);
-                }
-                bool statusUpdated = acceptedContract.Update(playerTime);
-                if (statusUpdated)
-                {
-                    // Check status and do something with it.
-                    if (acceptedContract.status == Contract.ContractStatus.Completed)
-                    {
-                        finishedContracts.Add(acceptedContract);
-                    }
-                }
-            }
-            // Cleanup accepted contracts
-            for (int acceptedContractIndex = 0; acceptedContractIndex < acceptedContracts.Count; acceptedContractIndex++)
-            {
-                if (this.acceptedContracts[acceptedContractIndex].status != Contract.ContractStatus.Accepted)
-                {
-                    this.acceptedContracts.RemoveAt(acceptedContractIndex);
-                    acceptedContractIndex--;
-                }
-            }
-        }
+        // Update contracts
+        this.UpdateContracts();
 
+        // Draw GUI
         var style = ImGui.GetStyle();
 
         // Contract Management Window with two panels: left fixed-width, right flexible
@@ -541,6 +500,121 @@ public class ContractManager
         {
             contractToWrite.WriteToFile(savePath);
         }
+    }
+
+    // Contract Management back-end functions
+    
+    private void UpdateContracts()
+    {
+        double playerTime = Program.GetPlayerTime();
+        if (playerTime - this._lastUpdateTime < this._updateInterval) { return; }
+
+        this._lastUpdateTime = playerTime;
+        Console.WriteLine($"[CM] Game time: {playerTime}s blueprints {this._contractBlueprints.Count} offered: {this.offeredContracts.Count} accepted: {this.acceptedContracts.Count} finished: {this.finishedContracts.Count}");
+
+        // offer contracts
+        this.OfferContracts(playerTime);
+
+        // auto-accept for now
+        if (this.maxNumberOfAcceptedContracts >= this.acceptedContracts.Count)
+        {
+            foreach (Contract.Contract offeredContract in this.offeredContracts)
+            {
+                if (offeredContract.status == Contract.ContractStatus.Offered)
+                {
+                    offeredContract.AcceptOfferedContract(playerTime);
+                    acceptedContracts.Add(offeredContract);
+                }
+            }
+            // Cleanup offered contracts
+            for (int offeredContractIndex = 0; offeredContractIndex < offeredContracts.Count; offeredContractIndex++)
+            {
+                if (this.offeredContracts[offeredContractIndex].status != Contract.ContractStatus.Offered)
+                {
+                    this.offeredContracts.RemoveAt(offeredContractIndex);
+                    offeredContractIndex--;
+                }
+            }
+        }
+
+        // Update accepted contracts
+        KSA.Vehicle currentVehicle = Program.ControlledVehicle;
+        foreach (Contract.Contract acceptedContract in this.acceptedContracts)
+        {
+            if (currentVehicle != null)
+            {
+                acceptedContract.UpdateStateWithVehicle(currentVehicle);
+            }
+            bool statusUpdated = acceptedContract.Update(playerTime);
+            if (statusUpdated)
+            {
+                // Check status and do something with it.
+                if (acceptedContract.status == Contract.ContractStatus.Completed)
+                {
+                    finishedContracts.Add(acceptedContract);
+                }
+                    
+                // Cleanup accepted contracts
+                for (int acceptedContractIndex = 0; acceptedContractIndex < acceptedContracts.Count; acceptedContractIndex++)
+                {
+                    if (this.acceptedContracts[acceptedContractIndex].status != Contract.ContractStatus.Accepted)
+                    {
+                        this.acceptedContracts.RemoveAt(acceptedContractIndex);
+                        acceptedContractIndex--;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OfferContracts(double playerTime)
+    {
+        if (this.offeredContracts.Count >= this.maxNumberOfOfferedContracts) { return; }
+
+        List<ContractBlueprint.ContractBlueprint> contractBlueprintsToOffer = this.GetContractBlueprintsToOffer();
+        Random randomGenerator = new Random();
+        while (contractBlueprintsToOffer.Count + this.offeredContracts.Count > this.maxNumberOfOfferedContracts)
+        {
+            // Randomly select a subset of contracts to be offered
+            contractBlueprintsToOffer.RemoveAt(randomGenerator.Next(0, contractBlueprintsToOffer.Count));
+        }
+        foreach (ContractBlueprint.ContractBlueprint contractBlueprint in contractBlueprintsToOffer)
+        {
+            this.offeredContracts.Add(new Contract.Contract(contractBlueprint, playerTime));
+        }
+    }
+        
+    private List<ContractBlueprint.ContractBlueprint> GetContractBlueprintsToOffer()
+    {
+        List<ContractBlueprint.ContractBlueprint> contractBlueprintsToOffer = new List<ContractBlueprint.ContractBlueprint>();
+        foreach (ContractBlueprint.ContractBlueprint contractBlueprint in this._contractBlueprints)
+        {
+            if (this.CanOfferContractFromBlueprint(in contractBlueprint))
+            {
+                contractBlueprintsToOffer.Add(contractBlueprint);
+            }
+        }
+        return contractBlueprintsToOffer;
+    }
+
+    private bool CanOfferContractFromBlueprint(in ContractBlueprint.ContractBlueprint contractBlueprint)
+    {
+        bool canOfferContract = true;
+        foreach (ContractBlueprint.Prerequisite prerequisite in contractBlueprint.prerequisites)
+        {
+            if (prerequisite.type == PrerequisiteType.MaxNumOfferedContracts && this.offeredContracts.Count >= prerequisite.maxNumOfferedContracts)
+            {
+                canOfferContract = false;
+                break;
+            }
+            if (prerequisite.type == PrerequisiteType.MaxNumAcceptedContracts && this.acceptedContracts.Count >= prerequisite.maxNumAcceptedContracts)
+            {
+                canOfferContract = false;
+                break;
+            }
+            // TODO: Add a check if the contract was recently offered and rejected.
+        }
+        return canOfferContract;
     }
 }
 
